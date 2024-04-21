43367d5b51cd97d382e4ad46680d42808670daf2
feryele <mejriferyel431@gmail.com>
2024-04-21 18:56:19 +0100

    init projet

---
+++ src/main/java/com/pfa/brainboost/BrainboostApplication.java
@@ -0,0 +1,13 @@
+package com.pfa.brainboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainboostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainboostApplication.class, args);
+	}
+
+}
